1b1df805d1b3500fe39505de7627473e329ccc6f
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-06-26 20:45:58 -0400

    Organized code with functions

--- blank.js
+++ blank.js
@@ -1,33 +1,23 @@
-let gridContainer = document.querySelector(".grid");
+createGrid(16);
 
-for (let i = 0; i < 16; i++) {
-    let newBoxContainer = document.createElement("div");
-    newBoxContainer.classList.add("box-container")
-
-    for (let v = 0; v < 16; v++) {
-        let newBox = document.createElement("div");
-        newBox.classList.add("box")
-        newBox.setAttribute("data-value", 0);
-        newBoxContainer.appendChild(newBox);
-    }
-
-    gridContainer.appendChild(newBoxContainer);
-}
-
-
-gridContainer.addEventListener("mouseover", (event) => {
+function changeBox (event) {
     let box = event.target
     let red = Math.random() * 255;
     let green = Math.random() * 255;
     let blue = Math.random() * 255;
-    let opacity = Number(box.getAttribute("data-value"));
-    opacity = opacity + 0.1;
-    console.log(opacity);
-    box.setAttribute("data-value", opacity);
+    let opacity;
+
+    if (box.classList.contains("box")) {
+        opacity = Number(box.getAttribute("data-value"));
+        opacity = opacity + 0.1;
+        console.log(opacity);
+        box.setAttribute("data-value", opacity);
+    }
 
     if (box.classList.contains("box") && (box.classList.contains("filled") == false)) {
         box.classList.add("filled");
         box.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity}`;
+
     } else if (box.classList.contains("box") && (box.classList.contains("filled") == true)) {
         let colors = box.style.backgroundColor.split(",")
         colors.pop();
@@ -36,29 +26,18 @@ gridContainer.addEventListener("mouseover", (event) => {
         let newColors = colors.join();
         box.style.backgroundColor = newColors;
     }
-});
+}
 
-const button = document.querySelector("button");
-
-button.addEventListener("click", (event) => {
-    let userInput;
-    
-    do {
-        userInput = prompt("Enter a grid size between 1 - 100");
-    } while (userInput < 1 || userInput > 100);
-
-    let gridToBeRemoved = document.querySelector(".grid")
-    document.querySelector("body").removeChild(gridToBeRemoved);
-
+function createGrid (gridSize) {
     let newGrid = document.createElement("div");
     newGrid.classList.add("grid");
     document.querySelector("body").appendChild(newGrid);
 
-    for (let i = 0; i < userInput; i++) {
+    for (let i = 0; i < gridSize; i++) {
         let newBoxContainer = document.createElement("div");
         newBoxContainer.classList.add("box-container")
     
-        for (let v = 0; v < userInput; v++) {
+        for (let v = 0; v < gridSize; v++) {
             let newBox = document.createElement("div");
             newBox.classList.add("box")
             newBoxContainer.appendChild(newBox);
@@ -67,29 +46,21 @@ button.addEventListener("click", (event) => {
         newGrid.appendChild(newBoxContainer);
     }
 
-    let opacity = 0;
+    newGrid.addEventListener("mouseover", changeBox);
+}
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", (event) => {
+    let userInput;
+
+    do {
+        userInput = prompt("Enter a grid size between 1 - 100");
+    } while (userInput < 1 || userInput > 100);
+
+    let gridToBeRemoved = document.querySelector(".grid")
+    document.querySelector("body").removeChild(gridToBeRemoved);
+
+    createGrid(userInput);
     
-    newGrid.addEventListener("mouseover", (event) => {
-        let box = event.target
-        let red = Math.random() * 255;
-        let green = Math.random() * 255;
-        let blue = Math.random() * 255;
-        let opacity = Number(box.getAttribute("data-value"));
-        opacity = opacity + 0.1;
-        console.log(opacity);
-        box.setAttribute("data-value", opacity);
-
-        if (box.classList.contains("box") && (box.classList.contains("filled") == false)) {
-            box.classList.add("filled");
-            box.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity}`;
-        } else if (box.classList.contains("box") && (box.classList.contains("filled") == true)) {
-            let colors = box.style.backgroundColor.split(",")
-            colors.pop();
-            colors.push(opacity + ')');
-            
-            let newColors = colors.join();
-            box.style.backgroundColor = newColors;
-        }
-    });
-
 });
